c3bc1369530cdcb9f356fd05bb06eb1a9faaf7a4
mailanithin <mailanithin070@gmail.com>
2025-06-11 22:19:02 +0530

    added heading

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>i have created fullstack project </h1>
+      <h1>i have created fullstack project </h1>
+      
+      <h2>now i am pushing the code to remote repository</h2>
     </div>
   );
 }
